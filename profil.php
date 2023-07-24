076685d1c1eb2a8bbc1e08b37cbba0ef4ff8b83b
sugenz <140358959+sugenz@users.noreply.github.com>
2023-07-24 08:27:35 +0700

    Add files via upload

---
+++ profil.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sugendi Personal Website</title>
+    <style>
+        header
+        {
+            background-color: magenta;
+        }
+    </style>
+</head>
+
+<body>
+    <br>
+    <header style="text-align: center;">
+        <img src="IMG_4815.jpg"  width="200" height="200" style="border-radius: 50%;"/>
+        <h1>Sugendi</h1>
+        <p>(Web Developer)</p>
+    </header>
+
+    <hr />
+
+    <article style="text-align: center;">
+        <h2>Overview</h2>
+        <p>
+            Hi, saya Sugendi, saya adalah seorang mahasiswa yang sedang belajar menjadi web developer yang berdomisisli di Sambas.
+            Saat ini sedang berkuliah di POLTESA dan sedang mengikuti pelatihan Juior Web Developer.
+        </p>
+    </article>
+
+    <div style="max-width: 600px; margin: 3em auto">
+        <table border="1" width="100%">
+            <thead>
+                <tr style="background-color: orange;">
+                    <th>Skill</th>
+                    <th>Pengalaman</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>
+                        <ul>
+                            <li>HTML (Expert)</li>
+                            <li>CSS (Beginner)</li>
+                            <li>Javascript (Beginner)</li>
+                            <li>C++ (Beginner)</li>
+                            <li>Adobe Ilustrator (Expert)</li>
+                            <li>After Effect (Beginner)</li>
+
+                        </ul>
+                    </td>
+                    <td>
+                        <ul>
+                            <li>Freelancer di Internet</li>
+                            <li>Ikut pelatihan Desain Grafis Muda</li>
+                            <li>Magang di DPRD</li>
+                        </ul>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+    <hr>
+    <footer style="text-align: center;">
+        <p>Copyright &copy; 2023 Sugendi.</p>
+    </footer>
+</body>
+</html>
